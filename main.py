35ca23a288b9735cdf1f8a25c8bf18bfb592a48d
CaMiLo TuTa <camilotuta2004@gmail.com>
2022-10-18 21:42:59 -0500

    Tercer commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ i2c = I2C(0, scl=Pin(18), sda=Pin(19))
 ancho, alto = 128, 64
 oled = SSD1306_I2C(ancho, alto, i2c)
 
-# ? FUNCIÓN PRINCIPAL 
+# ? FUNCIÓN PRINCIPAL
 def main():
 
     # ? INSTANCIA CASILLAS (FOTORRESISTENCIAS)
@@ -24,6 +24,8 @@ def main():
     casilla8 = Pin(23, Pin.IN, Pin.PULL_UP)
     casilla9 = Pin(22, Pin.IN, Pin.PULL_UP)
     casilla10 = Pin(21, Pin.IN, Pin.PULL_UP)
+
+    casillaApostada = 0
 
     # ? ARRAYS PARA LA POSICION DE CADA CASILLA
     numeroCasillas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -51,8 +53,41 @@ def main():
     servo360 = PWM(Pin(19), freq=50)
 
     # ? PEDIR CASILLA PARA APOSTAR
-    casillaApostada0 = input()
-    casillaApostada = input("Tape la casilla que desea apostar: ")
+    print("TAPE UNA CASILLA PARA APOSTAR")
+
+    def casilla0Seleccionada(casi):
+        cas0 = 0
+        return (cas0)
+    def casilla1Seleccionada(casi):
+        cas1 = 1
+        return (cas1)
+    def casilla2Seleccionada(casi):
+        cas2 = 2
+        return (cas2)
+    def casilla3Seleccionada(casi):
+        cas3 = 3
+        return (cas3)
+    def casilla4Seleccionada(casi):
+        cas4 = 4
+        return (cas4)
+    def casilla5Seleccionada(casi):
+        cas5 = 5
+        return (cas5)
+    def casilla6Seleccionada(casi):
+        cas6 = 6
+        return (cas6)
+    def casilla7Seleccionada(casi):
+        cas7 = 7
+        return (cas7)
+    def casilla8Seleccionada(casi):
+        cas8 = 8
+        return (cas8)
+    def casilla9Seleccionada(casi):
+        cas9 = 9
+        return (cas9)
+    def casilla10Seleccionada(casi):
+        cas10 = 10
+        return (cas10)
 
     # ? DEFINICIÓN DE FUNCIONES PARA CADA RESULTADO
     def resultadoCasilla0():
@@ -60,18 +95,17 @@ def main():
         oled.text("   CASILLA 0", 0, 10)
         oled.text("     VERDE", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
-        oled.fill()
 
     def resultadoCasilla1():
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 1", 0, 10)
         oled.text("     ROJA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
@@ -81,7 +115,7 @@ def main():
         oled.text("   CASILLA 2", 0, 10)
         oled.text("     NEGRA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
@@ -91,7 +125,7 @@ def main():
         oled.text("   CASILLA 3", 0, 10)
         oled.text("     ROJA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
@@ -101,7 +135,7 @@ def main():
         oled.text("   CASILLA 4", 0, 10)
         oled.text("     NEGRA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
@@ -111,7 +145,7 @@ def main():
         oled.text("   CASILLA 5", 0, 10)
         oled.text("     ROJA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
@@ -121,7 +155,7 @@ def main():
         oled.text("   CASILLA 6", 0, 10)
         oled.text("     NEGRA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
@@ -131,7 +165,7 @@ def main():
         oled.text("   CASILLA 7", 0, 10)
         oled.text("     ROJA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
@@ -141,7 +175,7 @@ def main():
         oled.text("   CASILLA 8", 0, 10)
         oled.text("     NEGRA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
@@ -151,7 +185,7 @@ def main():
         oled.text("   CASILLA 9", 0, 10)
         oled.text("     ROJA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
@@ -161,13 +195,12 @@ def main():
         oled.text("   CASILLA 10", 0, 10)
         oled.text("     NEGRA", 0, 20)
         oled.text("USTED APOSTO LA ", 0, 30)
-        oled.text("       "+casillaApostada, 0, 40)
+        oled.text("       "+str(casillaApostada), 0, 40)
         oled.text("*"*16, 0, 50)
         oled.show()
         time.sleep(15)
 
     # ? DEFINICIÓN DE FUNCIONES PARA MOVIMIENTO DE CADA SERVO
-
     repe = 0
     def movimientoServo180():
         while repe in range(0, 2):
@@ -188,6 +221,44 @@ def main():
 
 # ? CICLO INFINITO
     while True:
+        # ~ SELECCIÓN DE CASILLAS
+        if casilla0.value() == 1:
+            casillaApostada = casilla0Seleccionada(casillaApostada)
+
+        if casilla1.value() == 1:
+            casillaApostada = casilla1Seleccionada(casillaApostada)
+
+        if casilla2.value() == 1:
+            casillaApostada = casilla2Seleccionada(casillaApostada)
+
+        if casilla3.value() == 1:
+            casillaApostada = casilla3Seleccionada(casillaApostada)
+
+        if casilla4.value() == 1:
+            casillaApostada = casilla4Seleccionada(casillaApostada)
+
+        if casilla5.value() == 1:
+            casillaApostada = casilla5Seleccionada(casillaApostada)
+
+        if casilla6.value() == 1:
+            casillaApostada = casilla6Seleccionada(casillaApostada)
+
+        if casilla7.value() == 1:
+            casillaApostada = casilla7Seleccionada(casillaApostada)
+
+        if casilla8.value() == 1:
+            casillaApostada = casilla8Seleccionada(casillaApostada)
+
+        if casilla9.value() == 1:
+            casillaApostada = casilla9Seleccionada(casillaApostada)
+
+        if casilla10.value() == 1:
+            casillaApostada = casilla10Seleccionada(casillaApostada)
+
+
+        time.sleep(3)
+        print("Ya puede quitar el dedo")
+
         # ~ REPRODUCCIÓN CANCIÓN
         notas1()
 
@@ -197,53 +268,41 @@ def main():
         # ~ CICLO PARA MOVER EL SERVO DE 360°
         movimientoServo360()
 
-        time.sleep(15)
+        time.sleep(10)
 
         # ** VERIFICAR EN QUE CASILLA TERMINÓ CON IF
         if casilla0.value() == 1:
             resultadoCasilla0()
-        else:
-            pass
+
         if casilla1.value() == 1:
             resultadoCasilla1()
-        else:
-            pass
+
         if casilla2.value() == 1:
             resultadoCasilla2()
-        else:
-            pass
+
         if casilla3.value() == 1:
             resultadoCasilla3()
-        else:
-            pass
+
         if casilla4.value() == 1:
             resultadoCasilla4()
-        else:
-            pass
+
         if casilla5.value() == 1:
             resultadoCasilla5()
-        else:
-            pass
+
         if casilla6.value() == 1:
             resultadoCasilla1()
-        else:
-            pass
+
         if casilla7.value() == 1:
             resultadoCasilla7()
-        else:
-            pass
+
         if casilla8.value() == 1:
             resultadoCasilla8()
-        else:
-            pass
+
         if casilla9.value() == 1:
             resultadoCasilla9()
-        else:
-            pass
+
         if casilla10.value() == 1:
             resultadoCasilla10()
-        else:
-            pass
 
 
 # ? BOTÓN EJECUCIÓN
