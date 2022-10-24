18c5e8ff474d3df37026e0e5a1f09c95073670d8
CaMiLo TuTa <camilotuta2004@gmail.com>
2022-10-23 21:45:44 -0500

    Sexto Commit

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ from BLE import BLEUART
 import time
 
 # ? FUNCIÓN PRINCIPAL
-
-
 def main():
+
+    # ? INSTANCIA SERVOMOTORES
+    servo180 = PWM(Pin(2), freq=50)
+
+    motorPolo1 = PWM(Pin(19), 20000)
+    motorPolo2 = PWM(Pin(18), 20000)
 
     # ? INSTANCIA BLUETOOTH
     ble = bluetooth.BLE()
@@ -17,51 +21,26 @@ def main():
     uart = BLEUART(ble, name)
 
     # ? INSTANCIA PANTALLA OLED
-    i2c = I2C(0, scl=Pin(18), sda=Pin(19))
+    i2c = I2C(0, scl=Pin(4), sda=Pin(15))
     ancho, alto = 128, 64
     oled = SSD1306_I2C(ancho, alto, i2c)
 
     # ? INSTANCIA CASILLAS (FOTORRESISTENCIAS)
-    casilla0 = Pin(13, Pin.IN, Pin.PULL_UP)
-    casilla1 = Pin(14, Pin.IN, Pin.PULL_UP)
-    casilla2 = Pin(12, Pin.IN, Pin.PULL_UP)
-    casilla3 = Pin(27, Pin.IN, Pin.PULL_UP)
+    casilla0 = Pin(32, Pin.IN, Pin.PULL_UP)
+    casilla1 = Pin(13, Pin.IN, Pin.PULL_UP)
+    casilla2 = Pin(23, Pin.IN, Pin.PULL_UP)
+    casilla3 = Pin(33, Pin.IN, Pin.PULL_UP)
     casilla4 = Pin(26, Pin.IN, Pin.PULL_UP)
     casilla5 = Pin(25, Pin.IN, Pin.PULL_UP)
-    casilla6 = Pin(33, Pin.IN, Pin.PULL_UP)
-    casilla7 = Pin(32, Pin.IN, Pin.PULL_UP)
-    casilla8 = Pin(23, Pin.IN, Pin.PULL_UP)
-    casilla9 = Pin(22, Pin.IN, Pin.PULL_UP)
-    casilla10 = Pin(21, Pin.IN, Pin.PULL_UP)
-
-    casillaApostada = 0
-
-    # ? ARRAYS PARA LA POSICION DE CADA CASILLA
-    numeroCasillas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    colorCasillas = ["VERDE", "ROJA", "NEGRA", "ROJA", "NEGRA",
-                     "ROJA", "NEGRA", "ROJA", "NEGRA", "ROJA", "NEGRA"]
-
-    # ? ARRAYS PARA EL VALOR DE CADA CASILLA
-    casillasValor = [casilla0.value(), casilla1.value(), casilla2.value(), casilla3.value(), casilla4.value(
-    ), casilla5.value(), casilla6.value(), casilla7.value(), casilla8.value(), casilla9.value(), casilla10.value()]
-
-   # ? INSTANCIA BUZZER
-    buzzer = PWM(Pin(5))
-    notas1 = [369, 277, 329, 369, 329, 277, 493, 277, 369, 277, 329, 369, 329, 277, 493, 277, 369, 261, 329,
-              369, 329, 277, 493, 277, 369, 277, 329, 369, 329, 277
-              ]
-
-    def notas1():
-        for i in notas1:
-            buzzer.freq(i)
-            time.sleep(0.2)
-
-    # ? INSTANCIA SERVOMOTORES
-    servo180 = PWM(Pin(18), freq=50)
-    servo360 = PWM(Pin(19), freq=50)
+    casilla6 = Pin(22, Pin.IN, Pin.PULL_UP)
+    casilla7 = Pin(21, Pin.IN, Pin.PULL_UP)
+    casilla8 = Pin(12, Pin.IN, Pin.PULL_UP)
+    casilla9 = Pin(27, Pin.IN, Pin.PULL_UP)
+    casilla10 = Pin(14, Pin.IN, Pin.PULL_UP)
 
     # ? PEDIR CASILLA PARA APOSTAR
-    print("TAPE UNA CASILLA PARA APOSTAR")
+    casillaApostada = 0
+
 
     def casilla0Seleccionada(casi):
         cas0 = 0
@@ -109,6 +88,7 @@ def main():
 
     # ? DEFINICIÓN DE FUNCIONES PARA CADA RESULTADO
     def resultadoCasilla0():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 0", 0, 10)
         oled.text("     VERDE", 0, 20)
@@ -119,6 +99,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla1():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 1", 0, 10)
         oled.text("     ROJA", 0, 20)
@@ -129,6 +110,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla2():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 2", 0, 10)
         oled.text("     NEGRA", 0, 20)
@@ -139,6 +121,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla3():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 3", 0, 10)
         oled.text("     ROJA", 0, 20)
@@ -149,6 +132,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla4():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 4", 0, 10)
         oled.text("     NEGRA", 0, 20)
@@ -159,6 +143,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla5():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 5", 0, 10)
         oled.text("     ROJA", 0, 20)
@@ -169,6 +154,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla6():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 6", 0, 10)
         oled.text("     NEGRA", 0, 20)
@@ -179,6 +165,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla7():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 7", 0, 10)
         oled.text("     ROJA", 0, 20)
@@ -189,6 +176,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla8():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 8", 0, 10)
         oled.text("     NEGRA", 0, 20)
@@ -199,6 +187,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla9():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 9", 0, 10)
         oled.text("     ROJA", 0, 20)
@@ -209,6 +198,7 @@ def main():
         time.sleep(15)
 
     def resultadoCasilla10():
+        oled.fill(0)
         oled.text("*"*16, 0, 0)
         oled.text("   CASILLA 10", 0, 10)
         oled.text("     NEGRA", 0, 20)
@@ -218,36 +208,110 @@ def main():
         oled.show()
         time.sleep(15)
 
-    # ? DEFINICIÓN DE FUNCIONES PARA MOVIMIENTO DE CADA SERVO
-    def mapearServo180(valor_sensor, minimo_entrada, maximo_entrada, minimo_salida, maximo_salida):
-        valor_mapeado = (valor_sensor - minimo_entrada) * (maximo_salida -
-                                                           minimo_salida) / (maximo_entrada - minimo_entrada) + minimo_salida
+    
+    # ? DEFINICIÓN DE FUNCIONES PARA LA APUESTA
+    def casillaGana0():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("VERDE", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana1():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("ROJA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana2():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("NEGRA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana3():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("ROJA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana4():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("NEGRA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana5():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("ROJA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana6():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("NEGRA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana7():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("ROJA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana8():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("NEGRA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana9():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("ROJA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+    def casillaGana10():
+        oled.fill(0) 
+        oled.text("GANA"+str(casillaApostada), 15, 27, 1)
+        oled.text("NEGRA", 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+
+    def casillaPerder():
+        oled.fill(0) 
+        oled.text("PERDIO", 15, 27, 1)
+        oled.text("APOSTO"+str(casillaApostada), 37, 42, 1)
+        oled.show()
+        time.sleep(5)
+
+        
+    # ? POSICIÓN INICIAL DE LOS MOTORES
+    motorPolo1.duty(0)
+    motorPolo2.duty(0)
+
+    servo180.duty(90)
+
+    # ? MAPEO MOTORES
+    def map(valor_sensor, minimo_entrada, maximo_entrada, minimo_salida, maximo_salida):
+        valor_mapeado = (valor_sensor - minimo_entrada) * (maximo_salida - minimo_salida) / (maximo_entrada - minimo_entrada) + minimo_salida
         return valor_mapeado
-
-    def movimientoServo180():
-        def on_rx():
-            rx_recibe = uart.read().decode().strip()
-            dato = float(rx_recibe)
-
-            if dato == "abrir":
-                dato = 160
-                print("Abierto")
-
-            if dato == "cerrar":
-                dato = 90
-                print("Cerrado")
-
-        uart.irq(handler=on_rx)
-
-    def movimientoServo360():
-        velocidad = 0
-        duty = int((12.346*velocidad**2) + (7777.8*velocidad)+700000)
-        servo360.duty_ns(duty)
-        servo360.duty(True)
 
 
 # ? CICLO INFINITO
     while True:
+
+        print("TAPE UNA CASILLA PARA APOSTAR")
 
         # ~ SELECCIÓN DE CASILLAS
         if casilla0.value() == 1:
@@ -283,21 +347,56 @@ def main():
         if casilla10.value() == 1:
             casillaApostada = casilla10Seleccionada(casillaApostada)
 
-        time.sleep(3)
+        time.sleep(7)
         print("Ya puede quitar el dedo")
 
-        # ~ REPRODUCCIÓN CANCIÓN
-        notas1()
 
         # ~ CICLO PARA MOVER EL SERVO DE 180°
-        movimientoServo180()
+        def on_rx():
+            dato = 0
+            rx_recibe = uart.read().decode().strip()
+            dato = float(rx_recibe)
+
+            if dato == "A":
+                dato = int(map(180, 0, 180, 1150, 8100))
+                servo180.duty_u16(dato)
+                print("Abierto")
+
+            if dato == "C":
+                dato = int(map(90, 0, 180, 1150, 8100))
+                servo180.duty(dato)
+                print("Cerrado")
 
         # ~ CICLO PARA MOVER EL SERVO DE 360°
-        movimientoServo360()
+            i = 0
+            for i in range(700, 1000, 10):
+                motorPolo1.duty(i)
+                time.sleep(0.2)
+                print(i)
 
-        time.sleep(10)
+            for i in range(1000, 1023, 2):
+                motorPolo1.duty(i)
+                time.sleep(0.2)
+                print(i)
 
-        # ** VERIFICAR EN QUE CASILLA TERMINÓ CON IF
+            for i in range(1023, 600, -10):
+                motorPolo1.duty(i)
+                time.sleep(0.2)
+                print(i)
+
+            else:
+                uart.write("el valor no aplica")
+                print("El valor no aplica")
+
+            servo180.duty(90)
+            motorPolo1.duty(0)
+
+        uart.irq(handler=on_rx)
+
+
+        time.sleep(15)
+
+        # ** VERIFICAR EN QUE CASILLA TERMINÓ 
         if casilla0.value() == 1:
             resultadoCasilla0()
 
@@ -317,7 +416,7 @@ def main():
             resultadoCasilla5()
 
         if casilla6.value() == 1:
-            resultadoCasilla1()
+            resultadoCasilla6()
 
         if casilla7.value() == 1:
             resultadoCasilla7()
@@ -331,8 +430,63 @@ def main():
         if casilla10.value() == 1:
             resultadoCasilla10()
 
+        # ** VERIFICAR SI LA APUESTA GANÓ
+        if casillaApostada == 0 and casilla0.value()==1:
+            casillaGana0()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 1 and casilla1.value()==1:
+            casillaGana1()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 2 and casilla2.value()==1:
+            casillaGana2()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 3 and casilla3.value()==1:
+            casillaGana3()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 4 and casilla4.value()==1:
+            casillaGana4()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 5 and casilla5.value()==1:
+            casillaGana5()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 6 and casilla6.value()==1:
+            casillaGana6()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 7 and casilla7.value()==1:
+            casillaGana7()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 8 and casilla8.value()==1:
+            casillaGana8()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 9 and casilla9.value()==1:
+            casillaGana9()
+        else:
+            casillaPerder()
+
+        if casillaApostada == 10 and casilla10.value()==1:
+            casillaGana10()
+        else:
+            casillaPerder()
+
 
 # ? BOTÓN EJECUCIÓN
-button = Pin(15, Pin.IN, Pin.PULL_UP)
-if __name__ == "__main__" and button.value() == 0:
+if __name__ == "__main__":
     main()
